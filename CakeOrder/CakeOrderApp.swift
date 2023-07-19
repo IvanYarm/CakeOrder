c22b380dd303f826f12d17b5878241dc6e6a59b5
Ivan <ivan.yarmoliuk@gmail.com>
2023-07-19 07:08:55 -0700

    Initial Commit

---
+++ CakeOrder/CakeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeOrderApp.swift
+//  CakeOrder
+//
+//  Created by Ivan Yarmoliuk on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CakeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
